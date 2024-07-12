41d03cfd6f3ff6656b4869ca0c16f7ac31fd0b39
Omar Reda <omar.reda13231@gmail.com>
2024-07-13 01:42:43 +0200

    Restructured GameManager file to be a class and added the following functions : DrawCard, PrintDeck, ReadCSVFile

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import Card
+from Card import Card
+from GameManager import GameManager
 def main():
+    Game = GameManager()
+    Game.ReadCSVFile()
+    Player1Name, Player2Name = Game.StartGame()
+    Game.PrintDeck(Player1Name)
+    print(len(Game._AllCards))
+    Game.DrawCard(Player1Name)
+    Game.DrawCard(Player1Name)
+    Game.DrawCard(Player1Name)
+    Game.DrawCard(Player1Name)
+    Game.PrintDeck(Player1Name)
+    print(len(Game._AllCards))
+
+if __name__ == "__main__":
+    main()
