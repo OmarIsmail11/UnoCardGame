73b3259302b63681f0d829c1cc8c9e8da1b14e98
Omar Reda <omar.reda13231@gmail.com>
2024-07-13 00:02:08 +0200

    Utitlized used of emojis in colours and updated csv file reader to be UTF-8

--- GameManager.py
+++ GameManager.py
@@ -1,13 +1,13 @@
 import Card
 import csv
-
-with open("Cards.csv") as file:
+import emoji
+with open("Cards.csv", encoding="utf-8") as file:
     reader = csv.DictReader(file, fieldnames=["type", "colour", "number"])
     AllCards = []
     for row in reader:
         UnoCard = Card.Card(row["type"],row["colour"],row["number"])
         AllCards.append(UnoCard)
 
-print(AllCards)
-    
+print(AllCards[1:])
+
 
