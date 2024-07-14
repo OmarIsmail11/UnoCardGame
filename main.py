2602906b40c15123c2571a4251fca93e5e65604b
Omar Reda <omar.reda13231@gmail.com>
2024-07-14 04:03:25 +0200

    Finished PlayCard fn and changed name of old PlayCard fn to be UpdatePile, now all interactions between cards are implemented yet actions still aren't done

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from Card import Card
+import os
 from GameManager import GameManager
 def main():
     Game = GameManager()
     Game.ReadCSVFile()
     Game.PrintTutorial()
+    input("Press enter to continue: ")
+    os.system("cls||clear")
     Player1Name, Player2Name = Game.StartGame()
     Game.PlayGame()
-    
+
     # Game.DrawCard(Player1Name)
     # Game.DrawCard(Player1Name)
     # Game.PrintDeck(Player1Name)
